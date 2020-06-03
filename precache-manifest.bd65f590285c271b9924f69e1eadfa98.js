f15e082560e7b99f0350105995b9d82c260c3c67
mikolaj <mikolajrybinski@outlook.com>
2020-06-03 17:43:45 +0200

    Updates

---
+++ precache-manifest.bd65f590285c271b9924f69e1eadfa98.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "91c9922e309a691821e311f3757fff18",
+    "url": "www.sql-generator.pl/static/media/micon.91c9922e.woff2"
+  },
+  {
+    "revision": "11cb5835ea64439a5dfe",
+    "url": "www.sql-generator.pl/static/css/main.b820f4a1.chunk.css"
+  },
+  {
+    "revision": "ef3cf4deb8d679c67bae4116907849c2",
+    "url": "www.sql-generator.pl/static/media/micon.ef3cf4de.svg"
+  },
+  {
+    "revision": "4565db1e75cdbefa0c92",
+    "url": "www.sql-generator.pl/static/js/1.4565db1e.chunk.js"
+  },
+  {
+    "revision": "652a54c800ad1a6b46b2",
+    "url": "www.sql-generator.pl/static/js/runtime~main.652a54c8.js"
+  },
+  {
+    "revision": "11cb5835ea64439a5dfe",
+    "url": "www.sql-generator.pl/static/js/main.11cb5835.chunk.js"
+  },
+  {
+    "revision": "d03687b61ea7e10c9064205236581078",
+    "url": "www.sql-generator.pl/static/media/micon.d03687b6.woff"
+  },
+  {
+    "revision": "ccfd1d867128ac4a23d6d3a071764315",
+    "url": "www.sql-generator.pl/static/media/micon.ccfd1d86.ttf"
+  },
+  {
+    "revision": "3d929d7486d3aeab85ba0b97c94723a6",
+    "url": "www.sql-generator.pl/static/media/micon.3d929d74.eot"
+  },
+  {
+    "revision": "4565db1e75cdbefa0c92",
+    "url": "www.sql-generator.pl/static/css/1.132f00a5.chunk.css"
+  },
+  {
+    "revision": "91500c06c4b00b09144f467c64d152af",
+    "url": "www.sql-generator.pl/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01b2ac2969e29d2addf5f9ca73807186.js"
+  "www.sql-generator.pl/precache-manifest.bd65f590285c271b9924f69e1eadfa98.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("www.sql-generator.pl/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
