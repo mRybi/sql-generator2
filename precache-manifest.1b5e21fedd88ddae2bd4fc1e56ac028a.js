d34afde20c96183181e5004b5890350d14ec231b
mikolaj <mikolajrybinski@outlook.com>
2020-06-06 22:07:32 +0200

    Updates

---
+++ precache-manifest.1b5e21fedd88ddae2bd4fc1e56ac028a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "91c9922e309a691821e311f3757fff18",
+    "url": "/static/media/micon.91c9922e.woff2"
+  },
+  {
+    "revision": "86df5c3fb461c29c836a",
+    "url": "/static/css/main.58cf53d2.chunk.css"
+  },
+  {
+    "revision": "ef3cf4deb8d679c67bae4116907849c2",
+    "url": "/static/media/micon.ef3cf4de.svg"
+  },
+  {
+    "revision": "0a4ffa05afe659c28e46",
+    "url": "/static/js/1.0a4ffa05.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86df5c3fb461c29c836a",
+    "url": "/static/js/main.86df5c3f.chunk.js"
+  },
+  {
+    "revision": "d03687b61ea7e10c9064205236581078",
+    "url": "/static/media/micon.d03687b6.woff"
+  },
+  {
+    "revision": "3d929d7486d3aeab85ba0b97c94723a6",
+    "url": "/static/media/micon.3d929d74.eot"
+  },
+  {
+    "revision": "ccfd1d867128ac4a23d6d3a071764315",
+    "url": "/static/media/micon.ccfd1d86.ttf"
+  },
+  {
+    "revision": "0a4ffa05afe659c28e46",
+    "url": "/static/css/1.10784c3e.chunk.css"
+  },
+  {
+    "revision": "610910e393f8f32d7cddd0063022bc1d",
+    "url": "/index.html"
+  }
+];
